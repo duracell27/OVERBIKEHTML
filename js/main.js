c944ab823266f11539544086628d485aa8eebf4d
duracell27 <volodymyrshmidt@gmail.com>
2020-09-24 18:14:26 +0300

    product page ready

--- js/main.js
+++ js/main.js
@@ -53,3 +53,26 @@ jQuery(($) => {
         }
     });
 });
+
+$(document).ready(function () {
+    $(".owl-carousel").owlCarousel({
+        loop: true,
+        margin: 10,
+        nav: true,
+        dots: true,
+        navText: ["<img src='../img/sliderarrowleft.png'>","<img src='../img/sliderarrowright.png'>"],
+        dotsData: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 1
+            },
+            1000: {
+                items: 1
+            }
+        }
+    });
+});
+
